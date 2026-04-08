0e2717382df242dc0e4525e206449d3c8043ff6b
Mykhailo Kuznietsov <mkuznets@redhat.com>
2026-04-08 15:37:30 +0300

    chore: update release scripts with more parameters, and rename CI pipeline
    Signed-off-by: Mykhailo Kuznietsov <mkuznets@redhat.com>

--- make-release.sh
+++ make-release.sh
@@ -15,15 +15,26 @@ set -e
 
 usage ()
 {   echo "Usage: ./make-release.sh -v <version>"
+    echo "optional parameters:"
+    echo "--no-push - no pushes to remote repository"
+    echo "--skip-publish - no publishing to npmjs repository"
+    echo "--skip-bump-version - no updating of the next version after release"
     exit
 }
 
 init() {
   unset VERSION
 
+  SKIP_PUBLISH=0
+  SKIP_NEXT_VERSION_BUMP=0
+  NO_PUSH=0
+
   while [[ "$#" -gt 0 ]]; do
     case $1 in
       '-v'|'--version') VERSION="$2"; shift 1;;
+      '--no-push') NO_PUSH=1; shift 0;;
+      '--skip-publish') SKIP_PUBLISH=1; shift 0;;
+      '--skip-bump-version') SKIP_NEXT_VERSION_BUMP=1; shift 0;;
       '--help'|'-h') usage;;
     esac
     shift 1
@@ -34,6 +45,11 @@ init() {
   X_BRANCH=$(echo "${VERSION}" | sed 's/.$/x/')
   NEXT_BRANCH="pr-main-to-${VERSION}-next"
   NEXT_VERSION="${VERSION}-next"
+
+  echo "Running release script with following parameters:"
+  echo "no pushes to github: ${NO_PUSH}"
+  echo "no publishing to npmjs: ${SKIP_PUBLISH}"
+  echo "no bumping of the next version: ${SKIP_NEXT_VERSION_BUMP}"
 }
 
 resetChanges() {
@@ -56,8 +72,13 @@ checkoutToXBranch() {
   else
     echo "[INFO] ${X_BRANCH} does not exist. Will be created a new one from main."
     resetChanges "main"
-    git push origin main:"${X_BRANCH}"
-    git checkout "${X_BRANCH}"
+    if [[ ${NO_PUSH} -eq 0 ]]; then
+      git push origin main:"${X_BRANCH}"
+      git checkout "${X_BRANCH}"
+    else
+      echo "[INFO] Skipping pushing branch ${X_BRANCH} step"
+      git checkout -b "${X_BRANCH}"
+    fi
   fi
 }
 
@@ -70,22 +91,36 @@ checkoutToNextBranch() {
   else
     echo "[INFO] ${NEXT_BRANCH} does not exist. Will be created a new one from main."
     resetChanges "main"
-    git push origin main:"${NEXT_BRANCH}"
+    if [[ ${NO_PUSH} -eq 0 ]]; then
+      git push origin main:"${NEXT_BRANCH}"
+    else
+      echo "[INFO] Skipping pushing branch ${NEXT_BRANCH} step"
+    fi
     git checkout "${NEXT_BRANCH}"
   fi
 }
 
 publishArtifacts() {
-  echo "[INFO] Publish @eclipse-che/license-tool ${VERSION} artifacts"
+  echo "[INFO] Publishing @eclipse-che/license-tool ${VERSION} artifacts"
 
   npm ci
   npm run build
-  npm publish --tag latest --access public
+
+  if [[ ${SKIP_PUBLISH} -eq 0 ]]; then
+    echo "[INFO] Publishing @eclipse-che/license-tool ${VERSION} artifacts"
+    npm publish --tag latest --access public
+  else
+    echo "[INFO] Skipping publishing step"
+  fi
 }
 
 tagRelease() {
   git tag "${VERSION}"
-  git push origin "${VERSION}"
+  if [[ ${NO_PUSH} -eq 0 ]]; then
+    git push origin "${VERSION}"
+  else
+    echo "[INFO] Skipping pushing tag ${VERSION} step"
+  fi
 }
 
 createPR() {
@@ -102,16 +137,25 @@ updatePackageVersionAndCommitChanges() {
   local branch=$2
   local message=$3
 
-  echo "[INFO] Set ${version} in package.json"
+  local current_version=0
 
-  jq --arg version "${version}" '.version |= $version' package.json > package.json.update
-  mv -f package.json.update package.json
+  current_version=$(npm pkg get version)
+  if [[ "$current_version" == "\"$version\"" ]]; then
+    echo "[INFO] version is already specified in the package.json, skipping edits"
+  else
+    echo "[INFO] Setting ${version} in package.json"
 
-  echo "[INFO] Push changes to ${branch} branch"
+    jq --arg version "${version}" '.version |= $version' package.json > package.json.update
+    mv -f package.json.update package.json
 
-  git add package.json
-  git commit -s -m "${message}"
-  git push origin "${branch}"
+    git add package.json
+    git commit -s -m "${message}"
+    if [[ ${NO_PUSH} -eq 0 ]]; then
+        git push origin "${branch}"
+    else
+      echo "[INFO] Skipping pushing branch ${branch} step"
+    fi
+  fi
 }
 
 updateXBranch() {
@@ -124,8 +168,8 @@ updateXBranch() {
     "${X_BRANCH}" \
     "${COMMIT_MSG}"
 
-  tagRelease
   publishArtifacts
+  tagRelease
 }
 
 updateMainBranch() {
@@ -146,7 +190,11 @@ updateMainBranch() {
 
 run() {
   updateXBranch
-  updateMainBranch
+  if [[ ${SKIP_NEXT_VERSION_BUMP} -eq 0 ]]; then
+    updateMainBranch
+  else
+    echo "[INFO] Skipping next version bumping step"
+  fi
 }
 
 init "$@"
